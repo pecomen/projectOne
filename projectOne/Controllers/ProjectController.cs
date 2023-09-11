9e5a109783356e96b6c48ec4cb6f38451a7a41b8
pecomen <ortizpeace@gmail.com>
2023-09-11 22:29:08 +0100

    initial commit

---
+++ projectOne/Controllers/ProjectController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace projectOne.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+     
+
+        [HttpGet]
+        public IActionResult Get(
+            [FromQuery] string slack_name,
+            [FromQuery] string track)
+        {
+            // Get the current day of the week and UTC time
+            var currentDay = DateTime.UtcNow.ToString("dddd", CultureInfo.InvariantCulture);
+            var utcTime = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ");
+
+            // Construct the response JSON object
+            var response = new
+            {
+                slack_name = slack_name,
+                current_day = currentDay,
+                utc_time = utcTime,
+                track = track,
+                github_file_url = "https://github.com/username/repo/blob/main/file_name.ext",
+                github_repo_url = "https://github.com/username/repo",
+                status_code = 200
+            };
+
+            return Ok(response);
+        }
+    }
+}
+
